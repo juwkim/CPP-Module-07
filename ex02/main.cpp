8122842f6e3efbb80224dc246789b3a66b8933e6
juwkim <gimjuwon-ui-MacBookPro.local>
2023-08-30 15:02:43 +0900

    save

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: juwkim <juwkim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/30 13:58:25 by juwkim            #+#    #+#             */
-/*   Updated: 2023/08/30 15:00:28 by juwkim           ###   ########.fr       */
+/*   Updated: 2023/08/30 15:02:11 by juwkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,13 +29,16 @@ int main(int, char**)
     {
         Array<int> tmp = numbers;
         Array<int> test(tmp);
+        std::cout << "numbers: " << numbers << '\n';
+		std::cout << "tmp: " << tmp << '\n';
+		std::cout << "test: " << test << '\n';
     }
 
     for (int i = 0; i < MAX_VAL; i++)
     {
         if (mirror[i] != numbers[i])
         {
-            std::cerr << "didn't save the same value!!" << std::endl;
+            std::cerr << "didn't save the same value!!" << '\n';
             return 1;
         }
     }
